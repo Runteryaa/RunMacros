f08fd477e1a539c9e8bea4a869aa1305b22d4fc5
Runterya <97337724+Runteryaa@users.noreply.github.com>
2025-08-01 12:48:59 +0300

    Add files via upload

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -2,8 +2,6 @@ export const runtime = 'edge';
 
 import RecipeDetailClient from "@/components/RecipeDetailClient";
 
-type PageProps = { params: { id: string } };
-
-export default function RecipeDetailPage({ params }: PageProps) {
+export default function RecipeDetailPage({ params }: any) {
   return <RecipeDetailClient recipeId={params.id} />;
 }
